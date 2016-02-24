e4670e62373ac3e351c1c7f81e8a7cdd7f498ccc
RenanC <renan_campos@student.uml.edu>
2016-02-24 15:33:38 -0500

    Added data to repo to make testing easier

--- src/mutt.py
+++ src/mutt.py
@@ -50,56 +50,75 @@ def main():
     f_refs_10 = open(os.path.join(TMP_DIR, corruption + '_r10.json'), 'w')
     f_refs_20 = open(os.path.join(TMP_DIR, corruption + '_r20.json'), 'w')
 
-    f_sent_a.write('{\"info\":{}, \"licences\":[], \"type\":\"captions\", \"annotations\":[')
-    f_sent_b.write('{\"info\":{}, \"licences\":[], \"type\":\"captions\", \"annotations\":[')
-    f_refs_5.write('[')
-    f_refs_10.write('[')
-    f_refs_20.write('[')
+    f_sent_a.write('[')
+    f_sent_b.write('[')
+    f_refs_5.write( '{\"type\":\"captions\", \"info\":{}, \"licenses\":[], \"type\":\"captions\", \"annotations\":[')
+    f_refs_10.write('{\"type\":\"captions\", \"info\":{}, \"licenses\":[], \"type\":\"captions\", \"annotations\":[')
+    f_refs_20.write('{\"type\":\"captions\", \"info\":{}, \"licenses\":[], \"type\":\"captions\", \"annotations\":[')
 
     print corruption, len(entries)
     print "Gathering references for each entry in", corruption, "..."
     
-    # We only want entries with 20+ references
-    valid_corruptions = list()
+    ref_id_list  = [list(), list(), list()]
+    ref_id_count = [0,0,0]
+    comma =""
+
     for entry in entries: 
       
       cluster, refs = gather_references(entry[1], entry[0])
-      comma =""
 
+      # We only want entries with 20+ references
       if len(refs) >= 20:
         f_sent_a.write(comma)
         f_sent_b.write(comma)
-        f_sent_a.write(json.dumps({'image_id':cluster, 'caption':entry[2]}))
-        f_sent_b.write(json.dumps({'image_id':cluster, 'caption':entry[2]}))
+        f_sent_a.write(json.dumps({'image_id':hash(cluster), 'caption':entry[2]}))
+        f_sent_b.write(json.dumps({'image_id':hash(cluster), 'caption':entry[3]}))
         comma = ""
         for ref in refs[:5]:
           f_refs_5.write(comma)
-          f_refs_5.write(json.dumps({'image_id':cluster, 'caption':ref}))
+          #f_refs_5.write(json.dumps({'image_id':hash(cluster), 'id': ref_id_count[0], 'caption':ref}))
+          f_refs_5.write(json.dumps({'image_id':hash(cluster), 'id': hash(cluster), 'caption':ref}))
+          
+          ref_id_list[0].append({'id': hash(cluster)})
+          #ref_id_list[0].append({'id': ref_id_count[0]})
+          #ref_id_count[0] += 1
+          
           comma = ","
         comma = ""
         for ref in refs[:10]:
           f_refs_10.write(comma)
-          f_refs_10.write(json.dumps({'image_id':cluster, 'caption':ref}))
+          #f_refs_10.write(json.dumps({'image_id':hash(cluster), 'id': ref_id_count[1], 'caption':ref}))
+          f_refs_10.write(json.dumps({'image_id':hash(cluster), 'id': hash(cluster), 'caption':ref}))
+          
+          ref_id_list[1].append({'id': hash(cluster)})
+          #ref_id_list[1].append({'id': ref_id_count[1]})
+          #ref_id_count[1] += 1
+          
           comma = ","
         comma = ""
         for ref in refs[:20]:
           f_refs_20.write(comma)
-          f_refs_20.write(json.dumps({'image_id':cluster, 'caption':ref}))
+         #f_refs_20.write(json.dumps({'image_id':hash(cluster), 'id': ref_id_count[2], 'caption':ref}))
+          f_refs_20.write(json.dumps({'image_id':hash(cluster), 'id': hash(cluster), 'caption':ref}))
+          
+          ref_id_list[2].append({'id': hash(cluster)})
+          #ref_id_list[2].append({'id': ref_id_count[2]})
+          #ref_id_count[2] += 1
+          
           comma = ","
         comma = ","
-       
+ 
         break
-      print cluster, len(refs)
-      # dataset                 0
-      # original sentence       1
-      # sentence A              2
-      # sentence B (corruption) 3
-      # score                   4
-    f_sent_a.write(']}')
-    f_sent_b.write(']}')
-    f_refs_5.write(']')
-    f_refs_10.write(']')
-    f_refs_20.write(']')
+        # dataset                 0
+        # original sentence       1
+        # sentence A              2
+        # sentence B (corruption) 3
+        # score                   4
+    f_sent_a.write(']')
+    f_sent_b.write(']')
+    f_refs_5.write( '], \"images\":' + json.dumps(ref_id_list[0]) + '}')
+    f_refs_10.write('], \"images\":' + json.dumps(ref_id_list[1]) + '}')
+    f_refs_20.write('], \"images\":' + json.dumps(ref_id_list[2]) + '}')
  
     f_sent_a.close()
     f_sent_b.close()
